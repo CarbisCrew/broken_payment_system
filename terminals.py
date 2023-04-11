d5dac9e79f7fcc94d9eb07ac051f4ba7c1dc3adc
Danila Matyshev <gribnoysalatik@gmail.com>
2023-04-11 17:13:36 +0700

    add notification requirement

--- terminals.py
+++ terminals.py
@@ -19,10 +19,15 @@ class PaymentTerminal(ABC):
     def process_total_spent_operation(self, total_spent_account: IAccount):
         ...
 
+    @abstractmethod
+    def notify(self, citizen: Citizen):
+        ...
+
     def dispatch_operation(self, citizen: Citizen):
         self.process_cash_operation(citizen.cash_account)
         self.process_bonus_operation(citizen.bonus_account)
         self.process_total_spent_operation(citizen.total_spent_account)
+        self.notify(citizen)
 
 
 # Платежный терминал в кафе
@@ -37,12 +42,18 @@ class CafeTerminal(PaymentTerminal):
     def process_total_spent_operation(self, total_spent_account: IAccount):
         total_spent_account.accrue(self.__sum__)
 
+    def notify(self, citizen: Citizen):
+        print(f'Добро пожаловать в кафе! Спасибо за покупку на {self.__sum__} рублей! Приятного аппетита!')
+
 
 # Платежный терминал в кинотеатре
 class CinemaTerminal(CafeTerminal):
 
     def process_bonus_operation(self, bonus_account: IAccount):
         bonus_account.accrue(self.__sum__ * .15)
+
+    def notify(self, citizen: Citizen):
+        print(f'Добро пожаловать в кинотеатр! Спасибо за покупку на {self.__sum__} рублей! Приятного просмотра!')
 
 
 # Платежный терминал в комунальном сервисе
@@ -55,9 +66,13 @@ class UtilityServiceTerminal(CafeTerminal):
     def process_bonus_operation(self, bonus_account: IAccount):
         pass
 
+    def notify(self, citizen: Citizen):
+        print(f'Спасибо за добровольное пожертвование.')
+
 
 # Палтежный терминал на работе
 class JobTerminal(PaymentTerminal):
+    
     def process_cash_operation(self, cash_account: IAccount, citizen_name: str):
         cash_account.accrue(len(citizen_name) * 100)
 
@@ -67,6 +82,10 @@ class JobTerminal(PaymentTerminal):
     def process_total_spent_operation(self, total_spent_account: IAccount):
         pass
 
+    def notify(self, citizen: Citizen):
+        print(f'Поздравляем с зарплатой!')
+
     def dispatch_operation(self, citizen: Citizen, citizen_name: str):
         self.process_cash_operation(citizen.cash_account, citizen_name)
+        self.notify(citizen)
         
