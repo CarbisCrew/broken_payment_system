5ecfa5051c0ea9d00a6a32ea8e98a54265f09ac9
Ekaterina Zolotareva <zolaekaterina@gmail.com>
2023-04-17 18:27:17 +0700

    test

--- accounts.py
+++ accounts.py
@@ -12,62 +12,47 @@ class IAccount(ABC):
     def balance(self) -> int:
         return self.__balance__
 
+
+class DecreaseBalance(IAccount):
     @abstractmethod
-    def pay(self, sum: int):
+    def decrease_balance(self, sum: int):
         ...
 
+class IncreaseBalance(IAccount):
     @abstractmethod
-    def writeoff(self, sum: int):
-        ...
-
-    @abstractmethod
-    def accrue(self, sum: int):
+    def increase_balance(self, num:int):
         ...
 
 # Наличный счет
-class CashAccount(IAccount):
-    
-    def pay(self, sum: int):
+class CashAccount(DecreaseBalance, IncreaseBalance):
+
+    def decrease_balance(self, sum: int):
         if self.balance < sum:
             raise PaymentError('Недостаточно средств')
-        print(f'Оплата с наличного счета на сумму {sum} рублей')
+        print(f'Оплата или списание с наличного счета на сумму {sum} рублей')
         self.__balance__ -= sum
 
-    def writeoff(self, sum: int):
-        print(f'Списание с наличного счета на сумму {sum} рублей')
-        self.__balance__ -= sum
-
-    def accrue(self, sum: int):
+    def increase_balance(self, sum: int):
         print(f'Начисление на наличный счет на сумму {sum} рублей')
         self.__balance__ += sum
-       
+
 
 # Бонусный счет
-class BonusAccount(IAccount):
+class BonusAccount(DecreaseBalance, IncreaseBalance):
 
-    def pay(self, sum: int):
+    def decrease_balance(self, sum: int):
         if self.balance < sum:
             raise PaymentError('Недостаточно средств')
-        print(f'Оплата с бонусного счета на сумму {sum} рублей')
+        print(f'Оплата или списание с бонусного счета на сумму {sum} рублей')
         self.__balance__ -= sum
 
-    def writeoff(self, sum: int):
-        raise PaymentError(f'Изъятие средств с бонусного счета запрещено')
-
-    def accrue(self, sum: int):
+    def increase_balance(self, sum: int):
         print(f'Начисление на бонусный счет на сумму {sum} рублей')
         self.__balance__ += sum
 
 
-class TotalSpentAccount(IAccount):
+class TotalSpentAccount(IncreaseBalance):
 
-    def pay(self, sum: int):
-        raise PaymentError(f'Запрещено оплачивать покупки со счета потрат')
-
-    def writeoff(self, sum: int):
-        raise PaymentError(f'Запрещено списывать средства со счета потрат')
-
-    def accrue(self, sum: int):
+    def increase_balance(self, sum: int):
         print(f'Начисление на счет потрат на сумму {sum} рублей')
         self.__balance__ += sum
-        
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from citizen import Citizen
 from terminals import JobTerminal, CafeTerminal, CinemaTerminal, UtilityServiceTerminal
-
 if __name__ == "__main__":
 
     # Жил-был Джон
     john_doe = Citizen()
 
     # Пришел Джон на работу за зарплатой
-    JobTerminal(0).dispatch_operation(john_doe, 'Jonh Doe')
+    JobTerminal(0).process_cash_operation(john_doe, 'Jonh Doe')
+    print(john_doe.cash_account.balance)
 
     # Оплатил комуналку
-    UtilityServiceTerminal(100).dispatch_operation(john_doe)
+    UtilityServiceTerminal(100).process_cash_operation(john_doe)
 
     # Сходил в кино
-    CinemaTerminal(100).dispatch_operation(john_doe)
+    CinemaTerminal(100).process_cash_operation(john_doe)
 
     # И поужинал в кафе
-    CafeTerminal(100).dispatch_operation(john_doe)
-    
+    CafeTerminal(100).process_cash_operation(john_doe)
+    CafeTerminal(35).process_bonus_operation(john_doe)
+
     # И осталось у джона столько вот денег
     print(john_doe.cash_account.balance)
     print(john_doe.bonus_account.balance)
